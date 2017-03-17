01d33cd5174ccb23f8121cd1291d613bf2c959fa
MoJiongChang <gdshwhl007@gdshwhl007demac-mini.local>
2017-03-17 11:08:45 +0800

    Initial Commit

---
+++ CoreLocationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreLocationDemo
+//
+//  Created by gdshwhl007 on 17/3/17.
+//  Copyright © 2017年 莫炯昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreLocationDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreLocationDemo
+//
+//  Created by gdshwhl007 on 17/3/17.
+//  Copyright © 2017年 莫炯昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
